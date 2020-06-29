dafbcce0ab88684e6db58fd2373798fceb1825d1
pmbgit <assersohnm@hotmail.com>
2020-06-29 22:11:26 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit not changed all of "this" <code>src/App.js</code> and save to reload.
+          Edit maybe changed all of "this" <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
